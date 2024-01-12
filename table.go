96d0bcba4e214e83b720547924be9f5fec96d231
John Pritchard <logicalexistentialism@gmail.com>
2024-01-12 07:06:13 -0500

    pyrus malus

--- table.go
+++ table.go
@@ -16,7 +16,7 @@ const (
 	TableNameTheTempleOfAthena	TableName	= "the_temple_of_athena"
 	TableNameCommunication	TableName	= "communication"
 	TableNameInstrumentality	TableName	= "instrumentality"
-	TableNameThroughTheEyesOfPyrusMalus	TableName	= "through_the_eyes_of_pyrus_malus"
+	TableNamePyrusMalus	TableName	= "pyrus_malus"
 
 	TablePathExistentialism	TablePath	= "/syntelos /science /anthropology /existentialism"
 	TablePathPolitics	TablePath	= "/gegonen /politics"
@@ -24,7 +24,7 @@ const (
 	TablePathTheTempleOfAthena	TablePath	= "/gegonen /the temple of athena"
 	TablePathCommunication	TablePath	= "/syntelos /science /anthropology /communication"
 	TablePathInstrumentality	TablePath	= "/syntelos /science /anthropology /instrumentality"
-	TablePathThroughTheEyesOfPyrusMalus	TablePath	= "/gegonen /through the eyes of pyrus malus"
+	TablePathPyrusMalus	TablePath	= "/gegonen /through the eyes of pyrus malus"
 
 	TableLinkExistentialism	TableLink	= "https://drive.google.com/drive/folders/162g6-KM5dOkWvJX7NgMRoKJuz8rkwk96"
 	TableLinkPolitics	TableLink	= "https://drive.google.com/drive/folders/1uhgjgL8HBzRwCgP6pXpCwQqLZ0IC_pgJ"
@@ -32,7 +32,7 @@ const (
 	TableLinkTheTempleOfAthena	TableLink	= "https://drive.google.com/drive/folders/1nUpMgy9n-wHWN13hlXm-YZH5rojTUi0j"
 	TableLinkCommunication	TableLink	= "https://drive.google.com/drive/folders/1tXs2GNe1R9wCsKbj-bcCyeiscrK0F2K0"
 	TableLinkInstrumentality	TableLink	= "https://drive.google.com/drive/folders/1YqybUMCurpLc0WdTOuUjew7EujYGFDuG"
-	TableLinkThroughTheEyesOfPyrusMalus	TableLink	= "https://drive.google.com/drive/folders/1ODny7w7sTRbzQQGYMREZdQaKZ7lHJDuk"
+	TableLinkPyrusMalus	TableLink	= "https://drive.google.com/drive/folders/1ODny7w7sTRbzQQGYMREZdQaKZ7lHJDuk"
 )
 
 func IsTableName(name TableName) bool {
@@ -58,8 +58,8 @@ func (this TableName) Path() TablePath {
 		return TablePathCommunication
 	case TableNameInstrumentality:
 		return TablePathInstrumentality
-	case TableNameThroughTheEyesOfPyrusMalus:
-		return TablePathThroughTheEyesOfPyrusMalus
+	case TableNamePyrusMalus:
+		return TablePathPyrusMalus
 
 	default:
 		return ""
@@ -80,8 +80,8 @@ func (this TableName) Link() TableLink {
 		return TableLinkCommunication
 	case TableNameInstrumentality:
 		return TableLinkInstrumentality
-	case TableNameThroughTheEyesOfPyrusMalus:
-		return TableLinkThroughTheEyesOfPyrusMalus
+	case TableNamePyrusMalus:
+		return TableLinkPyrusMalus
 
 	default:
 		return ""
